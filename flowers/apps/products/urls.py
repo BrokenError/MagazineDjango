e2ed4ce63ba689423a8e0449ef29f96bc9d3f8ea
BrokenError <vvglvv1@gmail.com>
2023-05-16 15:30:18 +0300

    login and registration function moved

--- flowers/apps/products/urls.py
+++ flowers/apps/products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import *
+from apps.user.views import SignupLoginView
 
 urlpatterns = [
     path('', SignupLoginView.as_view(), name='magazine_home'),
