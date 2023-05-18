11bcb0819e4d942d8faad6f1b70f5642022242cd
BrokenError <vvglvv1@gmail.com>
2023-05-18 13:51:39 +0300

    added password change for user

--- flowers/apps/user/forms.py
+++ flowers/apps/user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from apps.user.models import User
 from .models import Profile
+from django.contrib.auth.forms import SetPasswordForm
 
 
 class SaveDataUser(forms.ModelForm):
@@ -25,3 +26,13 @@ class AddPhone(forms.ModelForm):
 
 class VerifyForm(forms.Form):
     code = forms.CharField(max_length=8, required=True, help_text='Введите смс, отправленное на ваш телефон')
+
+
+class UserChangePassword(SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'modal-inputs',
+                'autocomplete': 'off',
+            })
